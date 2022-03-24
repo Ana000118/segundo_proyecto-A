2fd55e0c54120efdfc639d769b7808025cece59b
Ana Karina Rojas Juan <rojasjuananakarina@gmail.com>
2022-03-24 10:01:30 -0600

    último commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
 function App() {
-  return (
-    <div className="App">
-      <h1>segundo_proyecto</h1>
-    </div>
-  );
-}
+  const persona = {
+    nombre: "Ana Karina",
+    apellidoPaterno: "Rojas",
+    apellidoMaterno: "Juan",
+    edad: 22,
 
+  };
+
+  const nuevoTelefono = "556 887 2222"
+
+  const { nombre, apellidoPaterno, apellidoMaterno, edad} = persona;
+  
+  const contactoPersona = {
+    telefono : "333 245 4532",
+    email : "qwery@gmail.com",
+    empresa : "ITTUX",
+  }
+
+  const infoPersona = { ...persona, ...contactoPersona, puesto: "Comerciante", nuevoTelefono };
+
+  /*
+  console.log (persona);
+  console.log (contactoPersona);
+  console.log (infoPersona);
+*/
+  const frutas = ["manzana", "naranja", "fresa", "pera"];
+  const vegetales = ["lechuga", "zanahoria", "alcachofa"];
+  const carnes = ["res", "cerdo", "pollo", "pescado"];
+
+  const alimentos =[
+    ...frutas,
+    ...vegetales,
+    ...carnes,
+    "Cualquier otro alimento"
+  ];
+
+  console.table (frutas);
+  console.table (vegetales);
+  console.table (carnes);
+  console.table (alimentos);
+  
+  return (
+  <div className = "App">
+    <h3>
+      {`Soy ${nombre} ${apellidoPaterno} ${apellidoMaterno}
+       y tengo ${edad} años.`}
+     </h3>
+  
+  </div>);
+}
 export default App;
